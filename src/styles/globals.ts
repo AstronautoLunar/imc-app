5a9d4f3c4d7931045a952fc87eb6a3a061715c56
Pyedrown <Pedrinho_fsf@hotmail.com>
2022-02-19 03:38:49 -0300

    Adicionado a paleta de cores do projeto

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -21,6 +21,31 @@ const styles = StyleSheet.create({
     }
 });
 
+const colors = {
+    dark: {
+        underWeight: '#266F8C',
+        aboutWeight: '#5A4B02',
+        normal: '#0D4031',
+        obesity: '#984D0B',
+        severeObesity: '#940A15'
+    },
+    normal: {
+        underWeight: '#73BCD9',
+        aboutWeight: '#F2CB05',
+        normal: '#26BF94',
+        obesity: '#F29849',
+        severeObesity: '#F24452'
+    },
+    light: {
+        underWeight: '#EBF5FA',
+        aboutWeight: '#FDEB91',
+        normal: '#95EAD2',
+        obesity: '#FCE9D9',
+        severeObesity: '#FCD4D7'
+    }
+}
+
 export {
-    styles
+    styles,
+    colors
 }
