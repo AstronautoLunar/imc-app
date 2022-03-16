75d7ca940e9e8cee9456a23555e9c12ac229161c
Pyedrown <Pedrinho_fsf@hotmail.com>
2022-03-16 03:33:30 -0300

    Corrigindo problema ao iniciar testes com Jest - Parte 2

--- src/__tests__/header.test.tsx
+++ src/__tests__/header.test.tsx
@@ -1,8 +1,14 @@
 import Header  from "../components/Header";
-import { render } from "@testing-library/react-native";
+// import { render } from "@testing-library/react-native";
+import renderer from "react-test-renderer";
 
+// test("Cabeçalho renderizado corretamente", () => {
+//     const { toJSON } = render(<Header/>);
+
+//     expect(toJSON()).toMatchSnapshot();
+// });
 test("Cabeçalho renderizado corretamente", () => {
-    const { toJSON } = render(<Header/>);
+    const HeaderJSON = renderer.create(<Header/>).toJSON();
 
-    expect(toJSON()).toMatchSnapshot();
+    expect(HeaderJSON).toMatchSnapshot();
 });
